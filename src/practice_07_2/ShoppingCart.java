e7ac0e95bc3eaa0aba7805d100311c4224deb584
Olecika Darii <olealea001@gmail.com>
2018-03-05 11:51:10 +0200

    practice 7.2

---
+++ src/practice_07_2/ShoppingCart.java
@@ -0,0 +1,28 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_07_2;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+
+        String custName = "Steve Smith";
+        int spaceIdx = custName.indexOf(" ");
+        String firstName= custName.substring(0,spaceIdx);
+        StringBuilder sb = new StringBuilder(firstName);
+        sb.append(" Smith");
+
+        System.out.println(sb);
+        
+        //Instantiate and initialize sb to firstName.
+        
+
+	// Put the full name back together, using StringBuilder append method.
+	// You can just enter the String literal for the last name.
+	// Print the full name.
+        
+	
+	// (Optional) Can you append the last name without using a String literal?
+
+
+    }
+}
